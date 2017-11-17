4f2064bc5acc367ae0b7040c46ea942092fa2f37
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-11-17 14:43:25 +0000

    rename module

---
+++ app/code/MiltonBayer/General/Setup/InstallData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MiltonBayer\General\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $setup->endSetup();
+    }
+}
