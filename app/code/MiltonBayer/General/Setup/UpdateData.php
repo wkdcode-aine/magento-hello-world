193c9e7946778e259cbde0f7b24c2273a98d8f3f
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-11-21 17:22:16 +0000

    testing

---
+++ app/code/MiltonBayer/General/Setup/UpdateData.php
@@ -0,0 +1,27 @@
+<?php
+namespace MiltonBayer\General\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    private $eavSetupFactory;
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        if (version_compare($context->getVersion(), '1.0.4', '<')) {
+        }
+
+        $setup->endSetup();
+    }
+}
