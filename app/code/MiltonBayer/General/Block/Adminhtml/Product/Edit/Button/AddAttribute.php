cfc88cbb14bc3228f0ebb7a16f979adcae83b580
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-11-22 09:15:01 +0000

    remove the add attributes button

---
+++ app/code/MiltonBayer/General/Block/Adminhtml/Product/Edit/Button/AddAttribute.php
@@ -0,0 +1,8 @@
+<?php
+    namespace MiltonBayer\General\Block\Adminhtml\Product\Edit\Button;
+
+    // removes the 'add attribute' button
+    class AddAttribute extends \Magento\Catalog\Block\Adminhtml\Product\Edit\Button\Generic
+    {
+
+    }
