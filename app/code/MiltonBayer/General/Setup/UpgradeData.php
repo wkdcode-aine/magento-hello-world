8c9e76709786f04ad09b0149e1df5a5e9ecbb153
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-11-24 16:00:49 +0000

    add the field to switch on door configurator

--- app/code/MiltonBayer/General/Setup/UpgradeData.php
+++ app/code/MiltonBayer/General/Setup/UpgradeData.php
@@ -5,6 +5,8 @@ use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
 
 class UpgradeData implements UpgradeDataInterface
 {
@@ -19,8 +21,23 @@ class UpgradeData implements UpgradeDataInterface
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-        if (version_compare($context->getVersion(), '1.0.2', '<')) {
 
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        if (version_compare($context->getVersion(), '1.0.3', '<')) {
+
+            $eavSetup->addAttribute(Category::ENTITY,
+               'show_door_confiurator', [
+                    'type'     => 'int',
+                    'label'    => 'Show Door Configurator',
+                    'input'    => 'text',
+                    'visible'  => true,
+                    'default'  => 1,
+                    'sort_order' => 3,
+                    'required' => false,
+                    'global'   => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                    'group'    => 'Display Settings',
+                ]);
         }
 
         $setup->endSetup();
