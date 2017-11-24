5db7901868e67bed799a39e6f013bb64843aff40
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-11-24 15:49:30 +0000

    getting towards adding on the fly

--- app/code/MiltonBayer/General/view/frontend/requirejs-config.js
+++ app/code/MiltonBayer/General/view/frontend/requirejs-config.js
@@ -3,13 +3,19 @@ var config = {
         '*': {
             'category_description': 'MiltonBayer_General/js/category/description',
             'product_list_toolbar_sorter' : 'MiltonBayer_General/js/product/list/toolbar/sorter',
-            'product_view_options_select': 'MiltonBayer_General/js/product/view/options/select'
+            'product_view_options_select': 'MiltonBayer_General/js/product/view/options/select',
+
+            'category_door_configurator': 'MiltonBayer_General/js/category/door-configurator'
         }
     },
     config: {
         mixins: {
             "Magento_Catalog/js/product/list/toolbar" : {
                 'MiltonBayer_General/js/product/list/toolbar-mixin': true
+            },
+            "MiltonBayer_General/js/category/door-configurator": {
+                "MiltonBayer_General/js/category/door-configurator/door-select-mixin": true,
+                "MiltonBayer_General/js/category/door-configurator/door-size-mixin": true,
             }
         }
     }
