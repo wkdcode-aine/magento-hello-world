eaa4e6fa4e7d75b7d715d4aa99eef54bcd993514
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-12-05 11:38:42 +0000

    show all options even when empty

--- app/code/MiltonBayer/General/Model/Layer/Filter/Attribute.php
+++ app/code/MiltonBayer/General/Model/Layer/Filter/Attribute.php
@@ -4,6 +4,38 @@
     class Attribute extends \Magento\CatalogSearch\Model\Layer\Filter\Attribute {
 
         /**
+         * @var \Magento\Framework\Filter\StripTags
+         */
+        private $tagFilter;
+
+        /**
+         * @param \Magento\Catalog\Model\Layer\Filter\ItemFactory $filterItemFactory
+         * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+         * @param \Magento\Catalog\Model\Layer $layer
+         * @param \Magento\Catalog\Model\Layer\Filter\Item\DataBuilder $itemDataBuilder
+         * @param \Magento\Framework\Filter\StripTags $tagFilter
+         * @param array $data
+         */
+        public function __construct(
+            \Magento\Catalog\Model\Layer\Filter\ItemFactory $filterItemFactory,
+            \Magento\Store\Model\StoreManagerInterface $storeManager,
+            \Magento\Catalog\Model\Layer $layer,
+            \Magento\Catalog\Model\Layer\Filter\Item\DataBuilder $itemDataBuilder,
+            \Magento\Framework\Filter\StripTags $tagFilter,
+            array $data = []
+        ) {
+            parent::__construct(
+                $filterItemFactory,
+                $storeManager,
+                $layer,
+                $itemDataBuilder,
+                $tagFilter,
+                $data
+            );
+            $this->tagFilter = $tagFilter;
+        }
+
+        /**
          * Apply attribute option filter to product collection
          *
          * @param \Magento\Framework\App\RequestInterface $request
@@ -26,5 +58,76 @@
                 ->addFilter($this->_createItem($label, $attributeValue));
 
             return $this;
+        }
+
+        /**
+         * Get data array for building attribute filter items
+         *
+         * @return array
+         * @throws \Magento\Framework\Exception\LocalizedException
+         */
+        protected function _getItemsData()
+        {
+            $attribute = $this->getAttributeModel();
+            /** @var \MiltonBayer\General\Model\ResourceModel\Fulltext\Collection $productCollection */
+            $productCollection = $this->getLayer()
+                ->getProductCollection();
+            // $optionsFacetedData = $productCollection->getFacetedData($attribute->getAttributeCode());
+
+            $values = $attribute->getSource()->getAllOptions(false);
+
+            $isAttributeFilterable =
+                $this->getAttributeIsFilterable($attribute) === static::ATTRIBUTE_OPTIONS_ONLY_WITH_RESULTS;
+
+            if (count($values) === 0 && !$isAttributeFilterable) {
+                return $this->itemDataBuilder->build();
+            }
+
+            $productSize = $productCollection->getSize();
+
+            $options = $attribute->getFrontend()
+                ->getSelectOptions();
+            foreach ($options as $option) {
+                $this->buildOptionData($option);
+            }
+
+            return $this->itemDataBuilder->build();
+        }
+
+        /**
+         * Build option data
+         *
+         * @param array $option
+         * @param boolean $isAttributeFilterable
+         * @param array $optionsFacetedData
+         * @param int $productSize
+         * @return void
+         */
+        private function buildOptionData($option)
+        {
+            $value = $this->getOptionValue($option);
+            if ($value === false) {
+                return;
+            }
+
+            $this->itemDataBuilder->addItemData(
+                $this->tagFilter->filter($option['label']),
+                $value,
+                1
+            );
+        }
+
+        /**
+         * Retrieve option value if it exists
+         *
+         * @param array $option
+         * @return bool|string
+         */
+        private function getOptionValue($option)
+        {
+            if (empty($option['value']) && !is_numeric($option['value'])) {
+                return false;
+            }
+            return $option['value'];
         }
     }
