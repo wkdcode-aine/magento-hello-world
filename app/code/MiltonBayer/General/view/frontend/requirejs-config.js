15e9e0bd2dfcca89543b0eb7c8c8cee7f5fdd82c
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-12-07 11:24:04 +0000

    register js

--- app/code/MiltonBayer/General/view/frontend/requirejs-config.js
+++ app/code/MiltonBayer/General/view/frontend/requirejs-config.js
@@ -4,7 +4,7 @@ var config = {
             'category_description': 'MiltonBayer_General/js/category/description',
             'product_list_toolbar_sorter' : 'MiltonBayer_General/js/product/list/toolbar/sorter',
             'product_view_options_select': 'MiltonBayer_General/js/product/view/options/select',
-
+            'price_filter': 'MiltonBayer_General/js/price_filter',
             'category_door_configurator': 'MiltonBayer_General/js/category/door-configurator'
         }
     },
