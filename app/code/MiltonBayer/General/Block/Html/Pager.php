0d1eceb3234aa2d724a5b8353bdd0f2a087192d6
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-12-07 13:57:22 +0000

    update pagination

---
+++ app/code/MiltonBayer/General/Block/Html/Pager.php
@@ -0,0 +1,7 @@
+<?php
+    namespace MiltonBayer\General\Block\Html;
+
+    class Pager extends \Magento\Theme\Block\Html\Pager
+    {
+        protected $_template = 'MiltonBayer_General::html/pager.phtml';
+    }
