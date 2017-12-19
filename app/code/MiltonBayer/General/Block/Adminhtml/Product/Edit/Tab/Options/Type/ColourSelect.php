079a125d013d83b8591c7e4e1a01313a465bcc66
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-12-19 10:09:16 +0000

    remove unneeded class

--- app/code/MiltonBayer/General/Block/Adminhtml/Product/Edit/Tab/Options/Type/ColourSelect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    namespace MiltonBayer\General\Block\Adminhtml\Product\Edit\Tab\Options\Type;
-
-    use Magento\Catalog\Block\Adminhtml\Product\Edit\Tab\Options\Type\AbstractType;
-
-    class ColourSelect extends AbstractType
-    {
-    }
